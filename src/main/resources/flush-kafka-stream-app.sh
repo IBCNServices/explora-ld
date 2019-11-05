cb50064a05403af0e5197d1a047ddd818b8f9b88
Leandro Ordonez Ante <leandro.ordonezante@ugent.be>
2019-11-05 11:47:39 +0100

    Add 'export PATH=/usr/lib/kafka/bin:/home/ibcn056/.npm/bin:/home/ibcn056/bin:/home/ibcn056/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin:/snap/bin' in flush-kafka-streams-app.sh

--- src/main/resources/flush-kafka-stream-app.sh
+++ src/main/resources/flush-kafka-stream-app.sh
@@ -1,3 +1,4 @@
+export PATH=/usr/lib/kafka/bin:$PATH
 kafka-streams-application-reset.sh --application-id cot-aq-ingestion-gh6 --to-earliest --input-topics cot.airquality
 kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic raw-airquality.no2.number
 kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic view-airquality.no2.number-gh6-min
