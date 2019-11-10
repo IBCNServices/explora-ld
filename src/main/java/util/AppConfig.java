16f9ae8006ad16414102b185147cbe8f1a69ecc4
Leandro Ordonez Ante <leandro.ordonezante@ugent.be>
2019-11-10 11:54:30 +0100

    Remove per-year aggregation

--- src/main/java/util/AppConfig.java
+++ src/main/java/util/AppConfig.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 public class AppConfig {
     public static List<String> SUPPORTED_AGGR = Arrays.asList("avg", "sum", "count");
     public static List<String> SUPPORTED_INTERVALS = Arrays.asList("5min", "1hour", "1day", "1week", "1month", "all");
-    public static List<String> SUPPORTED_RESOLUTIONS = Arrays.asList("min", "hour", "day", "month", "year");
+    public static List<String> SUPPORTED_RESOLUTIONS = Arrays.asList("min", "hour", "day", "month");
     public static List<Integer> SUPPORTED_GH_PRECISION = System.getenv("GEOHASH_PRECISION") != null ? Stream.of(System.getenv("GEOHASH_PRECISION").split(",")).map(gh -> Integer.parseInt(gh)).collect(Collectors.toList()): Arrays.asList(6,7);
     public static HashMap<String, String> TIME_RANGES = new HashMap<String, String>(){{
         put("5min", "min");
