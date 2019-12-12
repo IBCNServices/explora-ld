aaba072e2a642f3e962537d9493bfbf268572e71
Leandro Ordonez Ante <leandro.ordonezante@ugent.be>
2019-12-12 11:05:10 +0100

    Add script for simulating concurrent load on the system

--- src/main/resources/sim_api_load.sh
+++ src/main/resources/sim_api_load.sh
@@ -2,4 +2,4 @@ seq 1 200 | xargs -n1 -P10  sh -c 'START=$(date +%s.%N); \
 curl "http://10.10.139.5:30070/api/airquality/airquality.no2::number/aggregate/avg/snapshot?ts=1567295940000&src=tiles&res=min&gh_precision=6&bbox=51.311646,4.306641,51.168823,4.504395"; \
 END=$(date +%s.%N); \
 DIFF=$(echo "$END - $START" | bc); \
-echo $DIFF' 
+echo $DIFF >> parallel_exec_time.log'
